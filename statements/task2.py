6d3ff2bcb6f0a929547481635eefc8c8df28029d
pkhanna-msky <pkhanna@murraystate.edu>
2025-09-02 20:24:47 -0500

    Done with task 2

--- statements/task2.py
+++ statements/task2.py
@@ -1,16 +1,16 @@
 while True:
-    raw = input('Enter an integer: ')
+    next_input = input('Enter an integer: ')
     try:
-        n = int(raw)
+        n = int(next_input)
         break
     except ValueError:
         print('Error: please enter a whole number (e.g., -5, 0, 12). ')
 
 if n == 0:
-    mutiples = [0]
+    multiples = [0]
 else:
     step = abs(n)
-    mutiples = list(range(0, 100, step))
+    multiples = list(range(0, 100, step))
 
-print(', '.join(str(x) for x in mutiples))
+print(', '.join(str(x) for x in multiples))
 
