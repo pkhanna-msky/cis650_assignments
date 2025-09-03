09b89aecfcffeac6f2cc73d371e855fc3644c2e8
pkhanna-msky <pkhanna@murraystate.edu>
2025-09-02 20:48:16 -0500

    Done with task 3

--- statements/task3.py
+++ statements/task3.py
@@ -1,12 +1,12 @@
-num_items = int(input('Enter the number of items: '))
+number_items = int(input('Enter the number of items: '))
 total = 0.0
 
-for i in range(num_items + 1):
-    name = input(f'Enter the name of item {i} name: ')
-    unit_price = float(input(f'Enter the unit price of item {i} unit price: '))
-    quantity = int(input(f'Enter the quantity of item {i} quantity: '))
+for i in range(1, number_items + 1):
+    name = input(f'Item {i} name: ')
+    unit_price = float(input(f'Item {i} unit price: '))
+    quantity = int(input(f'Item {i} quantity: '))
     extended_price = unit_price * quantity
     total += extended_price
-    print(f'Item {i}: {name}, unit price: {unit_price}, quantity: {quantity}, extended price: {extended_price}')
+    print(f'Item {i}: {name}, unit price: {unit_price:.2f}, quantity: {quantity}, extended price: {extended_price:.2f}')
 
-print(f'Total price for all items: {total}')
+print(f'Total: {total:.2f}')
