58d4d62f1881c238c604094bb2da702ba76bd4eb
pkhanna-msky <pkhanna@murraystate.edu>
2025-09-10 11:27:03 -0500

    Done with task posts

---
+++ sets_dictionaries_tuples/posts.py
@@ -0,0 +1,5 @@
+post_dictionary = [ {
+	"post": "Big sale today!!! #Deals #Sale #sale 	#Coupons Visit our site,",
+	"topic": "promotions"},
+	"post": "Big sale today!!! #Deals #Sale #sale 	#Coupons Visit our site,",
+	"topic": "promotions"},
